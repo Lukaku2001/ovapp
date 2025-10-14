a075c53d81fa4c5ac7916ff7d48d7a0d9949a7b6
Lukaku2001 <lukakukarthus@gmail.com>
2025-10-14 16:01:28 +0200

    Add files via upload

---
+++ OVAppV2/src/main/java/com/example/OVApp/OvAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.OVApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvAppApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(OvAppApplication.class, args);
+    }
+}
